81edb2e9c1eda835cb61c48496c6f6b24546b5d1
Michael Friedrich <michael.friedrich@netways.de>
2018-12-20 17:05:40 +0100

    Bash and myself, we won't become friends

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOS_GH_ICINGA=("icinga2" "icingaweb2" "icinga-vagrant")
+REPOS_GH_ICINGA=( "icinga2" "icingaweb2" "icinga-vagrant" )
 REPOS_GL_ICINGA=()
 REPOS_GH_NET=()
 REPOS_GL_NET=()
@@ -9,7 +9,7 @@ GIT_EMAIL_ICINGA="michael.friedrich@icinga.com"
 
 mkdir -p $HOME/dev/icinga
 cd $HOME/dev/icinga
-for r in $REPOS_GH_ICINGA; do
+for r in ${REPOS_GH_ICINGA[@]}; do
 	test -d $r || git clone https://github.com/icinga/$r.git
 	cd $r
 	git config user.email $GIT_EMAIL_ICINGA
