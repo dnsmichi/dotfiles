b6aa968dccc1441f59294d0d62e9b15c859494a3
Michael Friedrich <michael.friedrich@netways.de>
2018-12-20 13:22:02 +0100

    Add more casks

--- brew.sh
+++ brew.sh
@@ -9,6 +9,12 @@ brew upgrade
 
 BREW_PREFIX=$(brew --prefix)
 
+brew tap caskroom/cask
+brew tap homebrew/services
+brew tap homebrew/fuse
+brew tap homebrew/gui
+brew tap homebrew/completions
+
 # coreutils
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
@@ -41,7 +47,7 @@ brew install nmap
 brew install socat
 brew install xz
 
-brew install git
+brew install git tig hub
 brew install lynx
 brew install rename
 brew install rlwrap
@@ -54,25 +60,52 @@ brew install qcachegrind --with-graphviz
 brew install ccache boost cmake bison flex yajl openssl@1.1 mysql-connector-c++
 brew install nagios-plugins
 
+# Database
 brew install mariadb
 ln -sfv /usr/local/opt/mariadb/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mariadb.plist
 
+# PHP & Apache
 brew install php
 
+sudo cp /etc/apache/httpd.conf /etc/apache/httpd.conf.bak
+
+cat >>/etc/apache/httpd.conf <<EOF
+LoadModule rewrite_module libexec/apache2/mod_rewrite.so
+LoadModule php7_module /usr/local/opt/php/lib/httpd/modules/libphp7.so
+<FilesMatch \.php$>
+   SetHandler application/x-httpd-php
+</FilesMatch>
+ 
+<IfModule dir_module>
+    DirectoryIndex index.php index.html
+</IfModule>
+EOF
+sudo apachectl start
+
+# Ruby
+brew install ruby
+
+# Python
+
+# Redis
 brew install redis
 brew services start redis
 
 ################################
 # Casks
-brew tap caskroom/cask
-
 brew cask install apache-directory-studio atom filezilla firefox macvim vlc wireshark tcpflow xquartz poedit
 
 # Gimp
 brew cask install gimp
 cd /Applications/GIMP.app/Contents/Resources/share/gimp/2.0/scripts/
 wget http://registry.gimp.org/files/arrow.scm
+
+# Vagrant
+brew cask install vagrant
+vagrant plugin install vagrant-parallels
+vagrant plugin install vagrant-openstack-provider
+vagrant plugin install vagrant-vbguest
 
 # final cleanup
 brew cleanup
