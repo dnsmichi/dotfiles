7bf94584474e60a877b770f4fd4e30352372b0c3
Michael Friedrich <michael.friedrich@netways.de>
2019-01-07 09:00:50 +0100

    Add some post bootstrap steps as docs

--- icinga2.sh
+++ icinga2.sh
@@ -24,3 +24,11 @@ brew services start redis
 
 # final cleanup
 brew cleanup
+
+# Bootstrap later
+# icinga2 api setup
+# cd /usr/local/icinga/icinga2/var/lib/icinga2/certs
+# HOST_NAME=mbpmif.int.netways.de
+# icinga2 pki new-cert --cn ${HOST_NAME} --csr ${HOST_NAME}.csr --key ${HOST_NAME}.key
+# icinga2 pki sign-csr --csr ${HOST_NAME}.csr --cert ${HOST_NAME}.crt
+# echo "const NodeName = \"${HOST_NAME}\"" >> /usr/local/icinga/icinga2/etc/icinga2/constants.conf
