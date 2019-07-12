a784b04e3fd460fdaca47de1ae2161daa6af8971
Michael Friedrich <michael.friedrich@netways.de>
2019-07-12 13:27:31 +0200

    icingaweb2: Add setup instructions

--- icingaweb2.sh
+++ icingaweb2.sh
@@ -54,5 +54,88 @@ brew services restart httpd
 # lsof -i :9000
 # /usr/local/opt/php/sbin/php-fpm -i
 
-# final cleanup
+
+# Setup
+./bin/icingacli setup config directory --group `id -g -n` --config /usr/local/icinga/icingaweb2/etc
+echo "Use the setup token and navigate to https://localhost/icingaweb2/setup"
+./bin/icingacli setup token create --config /usr/local/icinga/icingaweb2/etc
+#
+#mysql -e 'create database icingaweb2;'
+#mysql -e "grant all on icingaweb2.* to 'icingaweb2'@'localhost' identified by 'icingaweb2';"
+#mysql icingaweb2 < /usr/local/icinga/icingaweb2/etc/schema/mysql.schema.sql
+#
+#test -f /usr/local/icinga/icingaweb2/etc/config.ini || cat >/usr/local/icinga/icingaweb2/etc/config.ini <<EOF
+#[global]
+#show_stacktraces = "1"
+#show_application_state_messages = "1"
+#config_backend = "db"
+#config_resource = "icingaweb_db"
+#
+#[logging]
+#log = "php"
+#level = "ERROR"
+#application = "icingaweb2"
+#EOF
+#
+#test -f /usr/local/icinga/icingaweb2/etc/authentication.ini || cat >/usr/local/icinga/icingaweb2/etc/authentication.ini <<EOF
+#[icingaweb2]
+#backend = "db"
+#resource = "icingaweb_db"
+#EOF
+#
+#test -f /usr/local/icinga/icingaweb2/etc/roles.ini || cat >/usr/local/icinga/icingaweb2/etc/roles.ini <<EOF
+#[Administrators]
+#users = "icingaadmin"
+#permissions = "*"
+#groups = "Administrators"
+#EOF
+#
+#test -f /usr/local/icinga/icingaweb2/etc/resources.ini || cat >/usr/local/icinga/icingaweb2/etc/resources.ini <<EOF
+#[icingaweb_db]
+#type = "db"
+#db = "mysql"
+#host = "localhost"
+#port = ""
+#dbname = "icingaweb2"
+#username = "icingaweb2"
+#password = "icingaweb2"
+#charset = ""
+#use_ssl = "0"
+#
+#[icinga_ido]
+#type = "db"
+#db = "mysql"
+#host = "localhost"
+#port = ""
+#dbname = "icinga"
+#username = "icinga"
+#password = "icinga"
+#charset = ""
+#use_ssl = "0"
+#EOF
+#
+#test -f /usr/local/icinga/icingaweb2/etc/modules/monitoring/config.ini || cat >/usr/local/icinga/icingaweb2/etc/modules/monitoring/config.ini <<EOF
+#[security]
+#protected_customvars = "*pw*,*pass*,community"
+#EOF
+#
+#test -f /usr/local/icinga/icingaweb2/etc/modules/monitoring/backends.ini || cat >/usr/local/icinga/icingaweb2/etc/modules/monitoring/backends.ini <<EOF
+#[icinga]
+#type = "ido"
+#resource = "icinga_ido"
+#EOF
+#
+#test -f /usr/local/icinga/icingaweb2/etc/modules/monitoring/commandtransport.ini || cat >/usr/local/icinga/icingaweb2/etc/modules/monitoring/commandtransport.ini <<EOF
+#[icinga2]
+#transport = "api"
+#host = "localhost"
+#port = "5665"
+#username = "root"
+#password = "icinga"
+#EOF
+#
+#./bin/icingacli module enable monitoring
+
+# Finished
+
 brew cleanup
