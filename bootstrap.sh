6b0f8f915edac51811459ae9ae66a05fc6e39e77
Michael Friedrich <mfriedrich@gitlab.com>
2020-03-08 15:04:26 +0100

    Add bootstrap.sh for ZSH

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+cd ${0:a:h}
+
+git pull origin master
+
+function sync() {
+	rsync --exclude ".git/" \
+		--exclude "doc/" \
+		--exclude ".DS_Store" \
+		--exclude ".macos" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE" \
+		-avh --no-perms . ~;
+}
+
+sync
+
+unset sync;
