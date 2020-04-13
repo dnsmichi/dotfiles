83f0ef4c986cb9e84b9e01c12a81edde443b487c
Michael Friedrich <mfriedrich@gitlab.com>
2020-04-13 17:18:32 +0200

    git_prune: Add comment what it is actually doing

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,1 +1,2 @@
+# Delete all remote tracking Git branches where the upstream branch has been deleted
 alias git_prune="git fetch --prune && git branch -vv | grep 'origin/.*: gone]' | awk '{print \$1}' | xargs git branch -d"
