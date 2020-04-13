6e8247d1f2a91eb983de31b73e50a70caca72268
Michael Friedrich <mfriedrich@gitlab.com>
2020-04-13 17:52:47 +0200

    ZSH: Add genpw alias to generate a secure password

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,2 +1,5 @@
 # Delete all remote tracking Git branches where the upstream branch has been deleted
 alias git_prune="git fetch --prune && git branch -vv | grep 'origin/.*: gone]' | awk '{print \$1}' | xargs git branch -d"
+
+# Generate a secure password and copy it to clipboard
+alias genpw='LC_ALL=C tr -dc "[:alnum:]" < /dev/urandom | head -c 20 | pbcopy'
