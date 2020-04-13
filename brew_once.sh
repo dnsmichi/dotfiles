f8b6fc231ac51d2dc9b9cc9effbb686cdad3d033
Michael Friedrich <mfriedrich@gitlab.com>
2020-04-13 15:22:51 +0200

    brew_once.sh: Actually install Homebrew once

--- brew_once.sh
+++ brew_once.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # This installs homebrew itself, and also the command line tools in silent mode
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
 brew upgrade
